671fb1a83369d7219dcc13d0080e0efa934f0094
Huntleo <67329039+Reptegamer@users.noreply.github.com>
2024-12-20 17:33:57 -0600

    Create Reptegamer.java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Reptegamer.java
@@ -0,0 +1,16 @@
+public class Huntleo {
+
+    public static void main(String[] args) {
+    
+      //https://docs.oracle.com/javase/8/docs/api/
+      //Datos primitivos
+      int numero = 5;
+      flot decimal = 1.5;
+      boolean = true;
+      boolean = false;
+      //Variables
+      String variable = "Es una variable";
+        final String constante = "Es una constante";
+        System.out.println("Hola, Java");
+    }
+}
